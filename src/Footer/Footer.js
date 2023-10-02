294aa8782875170df36a1b55c9a5e8a9f6b2f3a0
matheezmujahid <matheez29@gmail.com>
2023-10-01 20:35:07 -0400

    Pushed code

---
+++ src/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+
+
+    <div className="center" aria-label="CopyRights Information">
+
+      All rights reserved &copy; @ Md Matheez Mujahid
+
+
+      <div className="h-card">
+
+        <p className="p-name">Matheez Mujahid</p>
+        &nbsp;
+
+        <a className="u-email" href="mailto:mmoham37@uncc.edu">Email me</a>&nbsp;
+
+        <p className="p-locality">NC</p>
+        
+        <p className="p-country-name">USA</p>
+        
+      </div>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
